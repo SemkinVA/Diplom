f7947ff10fbb25d50e2a46abcc0afe9ed110db1d
SemkinVA <127091360+SemkinVA@users.noreply.github.com>
2023-11-03 16:51:04 +0300

    Update 01-main.tf

--- terraform/01-main.tf
+++ terraform/01-main.tf
@@ -7,7 +7,7 @@ terraform {
   required_version = ">= 0.13"
 }
 provider "yandex" {
-  token     = "y0_AgAAAAABrtz7AATuwQAAAADvonraLRm6iO3aTcetalI1IDxyPphIAAo"
-  cloud_id  = "b1gp9h3u6teudun3llvb"
-  folder_id = "b1gthjthrl0bctlhqing"
+  token     = "-----"
+  cloud_id  = "-----"
+  folder_id = "-----"
 } 
